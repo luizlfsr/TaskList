5252934ba62312e9c9c4a195ec1a2dafbbb4b94b
luizlfsr <luizfelipelfsr@mail.com>
2023-11-18 01:13:09 -0300

    teste

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const taskContainer = document.querySelector('.tasks-container')
 const deleteButton = document.querySelector('.delete-button')
 const deleteTask = (taskItem) => {
     taskItem.remove();
-};
+}
 
 const validateInput = () => {
     return inputElement.value.trim().length > 0
@@ -41,10 +41,16 @@ const addTask = () => {
         divTaskItem.classList.add('task-item')
 
         textoTaskItem.textContent = valorInput
-        textoTaskItem.classList.add('task-texto')
+        textoTaskItem.classList.add('task-texto', 'incomplete-task')
+        //textoTaskItem.setAttribute('onclick', 'tarefaCompleta()')
 
         buttonTaskItem.textContent = 'deletar';
         buttonTaskItem.classList.add('delete-button')
+
+        divTaskItem.appendChild(textoTaskItem)
+        divTaskItem.appendChild(buttonTaskItem)
+    
+        inputElement.value = ''
 
 
         buttonTaskItem.addEventListener('click', () => {
@@ -52,17 +58,30 @@ const addTask = () => {
             deleteTask(taskItem);
         });
 
-        
-    
-        divTaskItem.appendChild(textoTaskItem)
-        divTaskItem.appendChild(buttonTaskItem)
-    
-        inputElement.value = ''
+
+        textoTaskItem.addEventListener('click', () => {
+            if (textoTaskItem.classList.contains('taskIncomplete')) {
+                textoTaskItem.classList.remove('taskIncomplete');
+                textoTaskItem.classList.add('taskComplete');
+                tarefaCompleta(textoTaskItem);
+            } else {
+                textoTaskItem.classList.add('taskIncomplete');
+                textoTaskItem.classList.remove('taskComplete');
+                tarefaIncompleta(textoTaskItem);
+            }
+        });
     
         return divTaskItem;
     }
 }
 
+const tarefaCompleta = (element) => {
+    element.style.textDecoration = "line-through"
+}
+
+const tarefaIncompleta = (element) => {
+    element.style.textDecoration = 'none'
+}
 
 addTaskButton.addEventListener('click', () => {
     handleAddTask();
